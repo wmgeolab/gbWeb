1ab5458e3958ef4acfa8ea678f546dc35c3fe221
DanRunfola <danr@wm.edu>
2021-05-27 13:28:35 -0400

    API migration step 1

---
+++ assets/python/apiBuild.py
@@ -0,0 +1,9 @@
+#gbWeb/api/currentRelease/ISO/ALL
+#gbWeb/api/currentRelease/all
+#gbWeb/api/currentRelease/ISO/ADM0
+#gbWeb/api/currentRelease/ISO/ADM1
+#gbWeb/api/currentRelease/ISO/ADM2
+
+#(etc).
+
+print("Hello gbWeb")
