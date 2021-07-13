d950d01de97bed4c641941bb8ebb761487446342
DanRunfola <danr@wm.edu>
2021-07-13 11:02:46 -0400

    bound

--- assets/python/statusTable.py
+++ assets/python/statusTable.py
@@ -12,13 +12,13 @@ ADMs = ["ADM0", "ADM1", "ADM2", "ADM3", "ADM4", "ADM5", "ADM6"]
 
 for bound in allBounds.iterrows():
     print(bound)
-    ISO = bound["Alpha-3code"]
+    ISO = bound[0]["Alpha-3code"]
     webJSON[ISO] = {}
     webJSON[ISO]["ISO"] = ISO
-    webJSON[ISO]["Name"] = bound["Name"]
-    webJSON[ISO]["Claimant(s)"] = bound["Claimant(s)"]
-    webJSON[ISO]["Disputed"] = bound["Disputed"]
-    webJSON[ISO]["Continent"] = bound["Continent"]
+    webJSON[ISO]["Name"] = bound[0]["Name"]
+    webJSON[ISO]["Claimant(s)"] = bound[0]["Claimant(s)"]
+    webJSON[ISO]["Disputed"] = bound[0]["Disputed"]
+    webJSON[ISO]["Continent"] = bound[0]["Continent"]
 
     for adm in ADMs:
         print(adm)
