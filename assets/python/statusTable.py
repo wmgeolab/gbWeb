40916524cf58029a4306c0b390651f86fe019190
DanRunfola <danr@wm.edu>
2021-07-13 11:44:01 -0400

    Debugging web build

--- assets/python/statusTable.py
+++ assets/python/statusTable.py
@@ -5,6 +5,8 @@ import sys
 allBounds = pd.read_csv("//__w/gbWeb/geoBoundaryBot/dta/iso_3166_1_alpha_3.csv", encoding='utf8').astype(str).dropna(axis=1,how='all')
 
 allOpen = requests.get("https://www.geoboundaries.org/api/current/gbOpen/ALL/ALL/").json()
+allHum = requests.get("https://www.geoboundaries.org/api/current/gbHumanitarian/ALL/ALL/").json()
+allAuth = requests.get("https://www.geoboundaries.org/api/current/gbAuthoritative/ALL/ALL/").json()
 
 #Convert our list into a dict
 allOpenDict = {}
@@ -18,6 +20,30 @@ for i in range(len(allOpen)):
     
     for key, value in allOpen[i].items():
         allOpenDict[ISO][ADM][key] = value
+
+allHumDict = {}
+for i in range(len(allHum)):
+    ISO = allHum[i]['boundaryISO']
+    ADM = allHum[i]['boundaryType']
+    if ISO not in allHumDict:
+        allHumDict[ISO] = {}
+    if ADM not in allHumDict[ISO]:
+        allHumDict[ISO][ADM] = {}
+    
+    for key, value in allHum[i].items():
+        allHumDict[ISO][ADM][key] = value
+
+allAuthDict = {}
+for i in range(len(allAuth)):
+    ISO = allAuth[i]['boundaryISO']
+    ADM = allAuth[i]['boundaryType']
+    if ISO not in allAuthDict:
+        allAuthDict[ISO] = {}
+    if ADM not in allAuthDict[ISO]:
+        allAuthDict[ISO][ADM] = {}
+    
+    for key, value in allAuth[i].items():
+        allAuthDict[ISO][ADM][key] = value
 
 webJSON = {}
 ADMs = ["ADM0", "ADM1", "ADM2", "ADM3", "ADM4", "ADM5", "ADM6"]
@@ -34,14 +60,50 @@ for _, bound in allBounds.iterrows():
     for adm in ADMs:
         if(adm in allOpenDict[ISO]):
             webJSON[ISO][adm] = {}
-            print(allOpenDict[ISO][adm])
-            webJSON[ISO][adm]["Available"] = "Yes"
-            
-            sys.exit()
+            webJSON[ISO][adm]["openAvailable"] = "Yes"
+            webJSON[ISO][adm]["ISO"] = ISO
+            webJSON[ISO][adm]["ADM"] = adm
+            webJSON[ISO][adm]["boundaryName"] = allOpenDict[ISO][adm]["boundaryName"]
+            webJSON[ISO][adm]["openBoundaryYearRepresented"] = allOpenDict[ISO][adm]["boundaryYearRepresented"]
+            webJSON[ISO][adm]["openBoundaryLicense"] = allOpenDict[ISO][adm]["boundaryLicense"]
+            webJSON[ISO][adm]["openSourceDataUpdateDate"] = allOpenDict[ISO][adm]["sourceDataUpdateDate"]
+            webJSON[ISO][adm]["openBoundarySource-1"] = allOpenDict[ISO][adm]["boundarySource-1"]
         elif((adm == "ADM0") or (adm == "ADM1") or (adm == "ADM2")):
-            webJSON[ISO][adm]["Available"] = "No"
+            webJSON[ISO][adm]["openAvailable"] = "No"
+        else:
+            pass
+
+        if(adm in allHumDict[ISO]):
+            if(adm not in webJSON[ISO]):
+                webJSON[ISO][adm] = {}
+            webJSON[ISO][adm]["humAvailable"] = "Yes"
+            webJSON[ISO][adm]["ISO"] = ISO
+            webJSON[ISO][adm]["ADM"] = adm
+            webJSON[ISO][adm]["boundaryName"] = allHumDict[ISO][adm]["boundaryName"]
+            webJSON[ISO][adm]["humBoundaryYearRepresented"] = allHumDict[ISO][adm]["boundaryYearRepresented"]
+            webJSON[ISO][adm]["humBoundaryLicense"] = allHumDict[ISO][adm]["boundaryLicense"]
+            webJSON[ISO][adm]["humSourceDataUpdateDate"] = allHumDict[ISO][adm]["sourceDataUpdateDate"]
+            webJSON[ISO][adm]["humBoundarySource-1"] = allHumDict[ISO][adm]["boundarySource-1"]
+        elif((adm == "ADM0") or (adm == "ADM1") or (adm == "ADM2")):
+            webJSON[ISO][adm]["humAvailable"] = "No"
+        else:
+            pass
+
+        if(adm in allAuthDict[ISO]):
+            if(adm not in webJSON[ISO]):
+                webJSON[ISO][adm] = {}
+            webJSON[ISO][adm]["authAvailable"] = "Yes"
+            webJSON[ISO][adm]["ISO"] = ISO
+            webJSON[ISO][adm]["ADM"] = adm
+            webJSON[ISO][adm]["boundaryName"] = allAuthDict[ISO][adm]["boundaryName"]
+            webJSON[ISO][adm]["authBoundaryYearRepresented"] = allAuthDict[ISO][adm]["boundaryYearRepresented"]
+            webJSON[ISO][adm]["authBoundaryLicense"] = allAuthDict[ISO][adm]["boundaryLicense"]
+            webJSON[ISO][adm]["authSourceDataUpdateDate"] = allAuthDict[ISO][adm]["sourceDataUpdateDate"]
+            webJSON[ISO][adm]["authBoundarySource-1"] = allAuthDict[ISO][adm]["boundarySource-1"]
+        elif((adm == "ADM0") or (adm == "ADM1") or (adm == "ADM2")):
+            webJSON[ISO][adm]["authAvailable"] = "No"
         else:
             pass
             
         
-        #loop
+        print(webJSON)
