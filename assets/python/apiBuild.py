9fe3b11ce4c721023d57fead4c1ef5760e79aefa
DanRunfola <danr@wm.edu>
2021-07-28 14:24:18 -0400

    LFS

--- assets/python/apiBuild.py
+++ assets/python/apiBuild.py
@@ -22,9 +22,8 @@ lfsFiles = []
 for i, r in gitatt.iterrows():
     splAtt = r[0].split("/")
     if(splAtt[0] == "releaseData"):
-        print(splAtt)
-        ISO = splAtt[2].split("_")[0]
-        ADM = splAtt[2].split("_")[1]
+        ISO = splAtt[2]
+        ADM = splAtt[3]
         lfsFiles.append(str(ISO + "_" + ADM))
 print(lfsFiles)
 sys.exit()
