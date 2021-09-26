009b055c6d907fdd2916ce6ac192f45f2245d4ad
CalvinBertonciniWM <clbertoncini@email.wm.edu>
2021-09-26 17:33:48 -0400

    Added description text; removed empty rows from table

--- assets/js/geoData.js
+++ assets/js/geoData.js
@@ -13,6 +13,8 @@ function read_geodata_data()
 
     var url = "https://raw.githubusercontent.com/wmgeolab/geoDataWeb/main/ancillaryData/gdOpen/sourceData/ADM0/" + selection_val + ".csv";
 
+    var meta_url = "https://raw.githubusercontent.com/wmgeolab/geoDataWeb/main/ancillaryData/gdOpen/sourceData/ADM0/meta" + selection_val + ".txt";
+
     console.log(url);
 
     var request = new XMLHttpRequest();
@@ -21,6 +23,15 @@ function read_geodata_data()
     request.send(null);
 
     var return_object = request.responseText;
+
+    var metadata_request = new XMLHttpRequest();
+    request.open("GET", meta_url, false);
+    request.send(null);
+    var metadata_text = request.responseText;
+
+    var desc = document.getElementById("metadata-text");
+
+    desc.innerHTML = metadata_text;
 
     var rows = return_object.split("\n");
 
@@ -31,6 +42,11 @@ function read_geodata_data()
     
     for (var i=0;i<rows.length;i++)
     {
+	if (rows[i] == "")
+	{
+	    continue
+	}
+	
 	console.log(rows[i]);
 	country = rows[i].split(",")[0];
 	data = rows[i].split(",")[1];
